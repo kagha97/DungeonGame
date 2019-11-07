c401cb5b926bc5a219243c1238011d2954adad68
wagm2720 <wagm2720@36drax.cs.uleth.ca>
2019-11-07 10:37:05 -0700

    Updated item placement and some room descriptions

--- include/GameConstants.h
+++ include/GameConstants.h
@@ -12,10 +12,12 @@ const char LOADGAME = 'L';
 
 
 const int ROOMCOUNT = 25;
+
+//TODO: Move these to external text file. Possibly in json format.
 const std::string ROOMDESC[ROOMCOUNT] = {
   "You are at the dungeon's entrance. The exit has been closed behind you.",
-  "",
-  "",
+  "This room is completely blank and devoid of detail. The only things you can see are the doors along its walls.",
+  "As you look around the room you see that the walls are painted with pictures of ducks. You feel a strange urge to tell the walls all of your problems, but you manage to restrain yourself."
   "",
   "",
   "",
@@ -42,15 +44,15 @@ const std::string ROOMDESC[ROOMCOUNT] = {
 
 const int KEYCOUNT = 4;
 // Decide which rooms to lock
-const int LOCKED[KEYCOUNT] = {5, 12, 20, 25};
+const int LOCKED[KEYCOUNT] = {5, 12, 20, 24};
 // Keys to unlock rooms
 // Room 5 first, then 25 and 20. 12 is in the centre of the map, so it'll be the last room.
 const Item KEYS[KEYCOUNT] = {
   Item("Rusty Key", Key, 5),
-  Item("Iron Key", Key, 25),
+  Item("Iron Key", Key, 24),
   Item("Shiny Key", Key, 20),
   Item("Gold Key", Key, 12)
 };
-const int KEYLOCATIONS[KEYCOUNT] = {23, 5, 25, 20};
+const int KEYLOCATIONS[KEYCOUNT] = {23, 5, 24, 20};
 
 #endif // GAMECONSTANTS_H
--- src/Room.cpp
+++ src/Room.cpp
@@ -2,52 +2,53 @@
 #include <string>
 #include <sstream>
 
-Room::Room(std::string desc)
-{
+Room::Room(std::string desc) {
   description = desc;
 }
 
-std::string Room::getDescription()
-{
+std::string Room::getDescription() {
   std::stringstream ss;
 
   ss << description << std::endl;
-  ss << "There are " << items.size() << " items in the room." << std::endl;
+  //ss << "There are " << items.size() << " items in the room." << std::endl;
+
+  if (items.size() > 0) {
+    ss << "Lying in the middle of the room, there is ";
+    //TODO: Better sentence for multiple items
+    for (int i = 0; i < items.size(); i++) {
+      ss << "a " << items[i].name;
+    }
+    ss << std::endl;
+  }
+
   ss << "There are " << npcs.size() << " NPCS in the room." << std::endl;
 
   return ss.str();
 }
-void Room::addNPC(NPC npc)
-{
+void Room::addNPC(NPC npc) {
   npcs.push_back(npc);
 }
 
-void Room::addItem(Item item)
-{
+void Room::addItem(Item item) {
   items.push_back(item);
 }
 
-void Room::interact(int npcIndex)
-{
+void Room::interact(int npcIndex) {
 
 }
 
-void Room::pickUpItem(int itemIndex)
-{
+void Room::pickUpItem(int itemIndex) {
 
 }
 
-void Room::setDescription(std::string desc)
-{
+void Room::setDescription(std::string desc) {
   description = desc;
 }
 
-Room::Room()
-{
+Room::Room() {
   //ctor
 }
 
-Room::~Room()
-{
+Room::~Room() {
   //dtor
 }
